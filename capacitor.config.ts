406594d71bcfee145c64271e337288e0f74f95d6
Patrick Taylor <pwt2104@hsutx.edu>
2025-02-07 21:03:38 -0600

    Added ability to develop in ios

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,15 +1,9 @@
-
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.2b671144109d4111bc325c5702dadfa1',
-  appName: 'flashclass-nectar',
+  appId: 'com.cowboycards',
+  appName: 'Cowboy Cards',
   webDir: 'dist',
-  server: {
-    url: 'https://2b671144-109d-4111-bc32-5c5702dadfa1.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
-  bundledWebRuntime: false
 };
 
 export default config;
